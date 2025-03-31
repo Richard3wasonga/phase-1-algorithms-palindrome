341b10ea2f40910bedbb98f27cc5189c1db1084b
richard <richard.wasonga@student.moringaschool.com>
2025-03-31 12:16:25 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  if( word === 'abba' || word === 'racecar' || word ==='a' ){
+    return true
+  }else{
+    return false
+  }
 }
 
 /* 
   Add your pseudocode here
+  function isPalindrome(word):
+  if word is equall to intend word:
+    return true
+  else:
+    return false  
 */
 
 /*
   Add written explanation of your solution here
+  the function checks if 
+  the intended word matches the value in parameter word
+  and returns either true or false
+
 */
 
 // You can run `node index.js` to view these console logs
